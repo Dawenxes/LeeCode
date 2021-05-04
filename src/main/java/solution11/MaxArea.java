fd6f5606bb08d034b7c1500c05922b0d70735be1
qingyao yang <yangqy@inhand.com.cn>
2021-05-04 11:28:24 +0800

    mac

---
+++ src/main/java/solution11/MaxArea.java
@@ -0,0 +1,18 @@
+package solution11;
+
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int ans = Integer.MIN_VALUE;
+        while (i < j) {
+            int area = Math.min(height[i], height[j]) * (j - i);
+            ans = Math.max(area, ans);
+            if (height[i] > height[j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return ans;
+    }
+}

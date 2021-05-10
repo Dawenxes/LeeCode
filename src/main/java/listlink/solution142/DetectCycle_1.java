af684d3ffc01c09e93f095d843697f6f1dd77f83
ZhangBoNeng <bromano@student.cccs.edu>
2021-05-10 22:41:35 +0800

    111

--- src/main/java/listlink/solution142/DetectCycle_1.java
+++ src/main/java/listlink/solution142/DetectCycle_1.java
@@ -2,8 +2,28 @@ package listlink.solution142;
 
 import common.ListNode;
 
-public class DetectCycle_1 {
-    public ListNode detectCycle(ListNode head) {
+import java.util.List;
 
+public class DetectCycle_1 {
+
+  public ListNode detectCycle(ListNode head) {
+    ListNode fast = head, slow = head;
+    while (fast != null) {
+      if (fast.next != null) {
+        fast = fast.next.next;
+      } else {
+        return null;
+      }
+      slow = slow.next;
+      if (slow == fast) {
+        ListNode ptr = head;
+        while (ptr != slow) {
+          ptr = ptr.next;
+          slow = slow.next;
+        }
+        return ptr;
+      }
     }
+    return null;
+  }
 }

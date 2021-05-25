b790f7f2ab00deea3ecc92b556c17be5199dab2a
ZhangBoNeng <bromano@student.cccs.edu>
2021-05-25 22:55:30 +0800

    111

--- src/main/java/qeque/solution84/LargestRectangleArea_1.java
+++ src/main/java/qeque/solution84/LargestRectangleArea_1.java
@@ -23,4 +23,24 @@ public class LargestRectangleArea_1 {
     }
     return ans;
   }
+
+  // 枚举
+  public int largestRectangleArea_1(int[] heights) {
+    Deque<Integer> stack = new LinkedList<>();
+    int[] right = new int[heights.length];
+    int[] left = new int[heights.length];
+    Arrays.fill(right, heights.length );
+    for (int i = 0; i < heights.length; i++) {
+      while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+        right[stack.pop()] = i;
+      }
+      left[i] = (stack.isEmpty() ? -1 : stack.peek());
+      stack.push(i);
+    }
+    int ans = 0;
+    for (int i = 0; i < heights.length; ++i) {
+      ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+    }
+    return ans;
+  }
 }
--- src/main/java/qeque/solution84/LargestRectangleArea__2.java
+++ src/main/java/qeque/solution84/LargestRectangleArea__2.java
@@ -6,6 +6,21 @@ import java.util.LinkedList;
 
 public class LargestRectangleArea__2 {
   public int largestRectangleArea(int[] heights) {
-
+    Deque<Integer> stack = new LinkedList<>();
+    int[] left = new int[heights.length];
+    int[] right = new int[heights.length];
+    for (int i = 0; i < heights.length; i++) {
+      while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+        right[stack.peek()] = i;
+        stack.pop();
+      }
+      left[i] = stack.isEmpty() ? -1 : stack.peek();
+      stack.push(i);
+    }
+    int result = 0;
+    for (int i = 0; i < heights.length; i++) {
+      result = Math.max(result, (right[i] - left[i] - 1) * heights[i]);
+    }
+    return result;
   }
 }

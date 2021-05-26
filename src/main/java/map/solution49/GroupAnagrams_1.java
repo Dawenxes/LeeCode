de48b39a39fb2ca9becf5a05eaec513107e56e4e
ZhangBoNeng <bromano@student.cccs.edu>
2021-05-26 22:53:24 +0800

    111

--- src/main/java/map/solution49/GroupAnagrams_1.java
+++ src/main/java/map/solution49/GroupAnagrams_1.java
@@ -1,10 +1,36 @@
 package map.solution49;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GroupAnagrams_1 {
   public List<List<String>> groupAnagrams(String[] strs) {
-    return new ArrayList<>();
+    Map<String, List<String>> map = new HashMap<>();
+    for (String str : strs) {
+      char[] chars = str.toCharArray();
+      int[] counts = new int[26];
+      for (int i = 0; i < chars.length; i++) {
+        counts[chars[i] - 'a']++;
+      }
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < 26; i++) {
+        if (counts[i] != 0) {
+          sb.append((char) ('a' + i));
+          sb.append(counts[i]);
+        }
+      }
+      String key = sb.toString();
+      List<String> orDefault = map.getOrDefault(key, new ArrayList<>());
+      orDefault.add(str);
+      map.put(key, orDefault);
+    }
+    return new ArrayList<List<String>>(map.values());
+  }
+
+  public static void main(String[] args) {
+    String[] str = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    new GroupAnagrams_1().groupAnagrams(str);
   }
 }

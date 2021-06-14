115e8f2c5d5f8879d7dd6519ade7d3dd6324c8f6
ZhangBoNeng <bromano@student.cccs.edu>
2021-06-14 23:26:27 +0800

    111

--- src/main/java/dfs_bfs/solution127/LadderLength_1.java
+++ src/main/java/dfs_bfs/solution127/LadderLength_1.java
@@ -1,10 +1,55 @@
 package dfs_bfs.solution127;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 public class LadderLength_1 {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+  Map<String, Integer> map = new HashMap<>();
+  List<List<Integer>> edge = new ArrayList<>();
+  int nodeNum = 0;
+  Queue<Integer> queue = new LinkedList<>();
 
-        return 0;
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    // 准备工作
+    for (String s : wordList) {
+      addEdge(s);
     }
+    if (!map.containsKey(endWord)) {
+      return -1;
+    }
+    addEdge(beginWord);
+    int[] dis = new int[nodeNum];
+    Arrays.fill(dis, Integer.MAX_VALUE);
+    int beginWordIndex = map.get(beginWord);
+    int endWordIndex = map.get(endWord);
+    dis[beginWordIndex] = 0;
+    queue.add(beginWordIndex);
+    while (!queue.isEmpty()) {
+      int index = queue.poll();
+      if (index == endWordIndex) {
+        return dis[endWordIndex] / 2 + 1;
+      }
+      List<Integer> edgeNode = edge.get(index);
+      for (Integer s : edgeNode) {
+        if (dis[s] == Integer.MAX_VALUE) {
+          dis[s] = dis[index] + 1;
+          queue.offer(s);
+        }
+      }
+    }
+    return 0;
+  }
+
+  private void addEdge(String s) {
+
+  }
+
+  private void addWord(String s) {
+
+  }
 }

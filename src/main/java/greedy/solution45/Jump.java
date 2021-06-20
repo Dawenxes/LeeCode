b1de31058fca7241fc5cc1745e9d0ab2c14dc061
ZhangBoNeng <bromano@student.cccs.edu>
2021-06-20 17:10:44 +0800

    111

--- src/main/java/greedy/solution45/Jump.java
+++ src/main/java/greedy/solution45/Jump.java
@@ -2,22 +2,20 @@ package greedy.solution45;
 
 public class Jump {
   public int jump(int[] nums) {
-    public int jump(int[] nums) {
-      int ans = 0;
-      int end = 0;
-      int maxSteps = 0;
-      for (int i = 0; i < nums.length-1; i++) {
-        if(i<=maxSteps){
-          maxSteps = Math.max(maxSteps, i + nums[i]);
-          if (i == end) {
-            ans++;
-            end = maxSteps;
-          }
-        }else{
-          return -1;
+    int ans = 0;
+    int end = 0;
+    int maxSteps = 0;
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (i <= maxSteps) {
+        maxSteps = Math.max(maxSteps, i + nums[i]);
+        if (i == end) {
+          ans++;
+          end = maxSteps;
         }
+      } else {
+        return -1;
       }
-      return ans;
     }
+    return ans;
   }
 }

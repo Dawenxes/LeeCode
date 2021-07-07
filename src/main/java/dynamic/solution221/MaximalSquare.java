0bf4db739581decfe08c6ebfaf3d1e84e5c493ff
ZhangBoneng <bromano@student.cccs.edu>
2021-07-07 18:01:17 +0800

    feat(no): day 210707

--- src/main/java/dynamic/solution221/MaximalSquare.java
+++ src/main/java/dynamic/solution221/MaximalSquare.java
@@ -1,17 +1,30 @@
 package dynamic.solution221;
 
 public class MaximalSquare {
-    public int maximalSquare(char[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-        int[][] dp = new int[r][c];
-        dp[0][0] =
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+    public static void main(String[] args) {
+        char[][] s = {{'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}};
+        new MaximalSquare().maximalSquare(s);
+    }
 
-                dp[i][j] = dp[]
+    public int maximalSquare(char[][] matrix) {
+        int r = matrix.length + 1;
+        int c = matrix[0].length + 1;
+        int[][] dp = new int[r][c];
+        int ans = 0;
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                char cc = matrix[i - 1][j - 1];
+                if (cc == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                ans = Math.max(dp[i][j] * dp[i][j], ans);
             }
         }
-        return dp[r][c];
+        return ans;
     }
 }

a462eb75a55881d7a51f9a42713b61bb6ef04f84
ZhangBoNeng <bromano@student.cccs.edu>
2021-10-23 15:39:21 +0800

    feat(nothing): 20210808

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,15 @@
+package example;
+
+/**
+ * @author ZhangBoneng
+ * @version 1.0.0
+ * @ClassName example.HelloWorld
+ * @Description TODO
+ * @createTime 2021年09月29日 19:09:00
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    //
+    System.out.println("Hello World!");
+  }
+}
